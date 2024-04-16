95c47c59018f81cb36bb3a24f510a96875f4f13a
javohirbekkhaydarov <javohirbekkhaydarov@gmail.com>
2024-04-16 11:25:08 +0500

    add texts

--- components/HeroSection.vue
+++ components/HeroSection.vue
@@ -1,10 +1,19 @@
 <template>
-<CarouselComponent />
+  <div class="hero_section flex justify-center">
+    <CarouselComponent />
+    <div
+      class="container max-w-[1290px] p-6 flex justify-start align-middle items-center"
+    >
+      <div class="hero_texts">
+        <div class="hero_title">
+          Забронируй отель Albatros Aqua Blu и плати только за двоих
+        </div>
+        <div class="hero_description">2 детей отдыхают бесплатно</div>
+      </div>
+    </div>
+  </div>
 </template>
 
-<script setup lang="ts">
-</script>
+<script setup lang="ts"></script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
