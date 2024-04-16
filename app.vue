cdd55da267aeee1ba13466c71ea9ac7d81e2e65f
javohirbekkhaydarov <javohirbekkhaydarov@gmail.com>
2024-04-16 12:11:22 +0500

    add seo config

--- app.vue
+++ app.vue
@@ -1,6 +1,16 @@
 <template>
     <NuxtPage />
 </template>
-<script setup>
-
+<script setup lang="ts">
+useSeoMeta({
+  title: 'Picktur',
+  ogTitle: 'Picktur',
+  description: 'Забронируй отель Albatros Aqua Blu и плати только за двоих',
+  ogDescription: 'Забронируй отель Albatros Aqua Blu и плати только за двоих',
+  ogImage: 'https://utfs.io/f/1c8bc384-b330-4f69-8696-806bd8982e51-1zbae.png',
+  ogSiteName: "Picktur",
+  ogImageWidth: 1200,
+  ogImageHeight: 630,
+  twitterCard: 'summary_large_image',
+})
 </script>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,7 +3,6 @@
 export default defineNuxtConfig({
   app: {
     head: {
-      title: "Picktur",
       charset: "utf-8",
       viewport: "width=device-width, initial-scale=1",
     },
