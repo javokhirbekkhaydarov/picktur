618a1c8972442eca438f709f2e86f11a6f0824b5
javohirbekkhaydarov <javohirbekkhaydarov@gmail.com>
2024-04-16 21:05:38 +0500

    add form heading

--- components/HeroForm.vue
+++ components/HeroForm.vue
@@ -1,15 +1,35 @@
 <template>
-<div class="form rounded-[16px]  bg-white p-[20px]">
-  Туры
-</div>
+  <div class="form rounded-[16px] bg-white p-[20px]">
+    <div class="form_heading flex justify-start gap-[44px]">
+      <div
+        v-for="(item, index) in items"
+        :key="index"
+        :class="{
+          active_heading_item: index === activeIndex,
+        }"
+        @click="setActive(index)"
+        class="flex gap-[4px] cursor-pointer form_heading_item"
+      >
+        <img :src="item.icon" alt="" />
+        <span>{{ item.label }}</span>
+      </div>
+    </div>
+  </div>
 </template>
 
-<script>
-export default {
-  name: "heroForm"
-}
+<script setup lang="ts">
+const items = [
+  { label: "Туры", icon: "/assets/icons/form/globus.svg" },
+  { label: "Отели", icon: "/assets/icons/form/hotel.svg" },
+  { label: "Горящие туры", icon: "/assets/icons/form/fire.svg" },
+  { label: "Авиабилеты (скоро)", icon: "/assets/icons/form/plane.svg" },
+];
+
+const activeIndex = ref(0);
+
+const setActive = (index: number) => {
+  activeIndex.value = index;
+};
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
